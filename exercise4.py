b4f43d66bd98734b3801e0a7673b6569043dad2d
gabegodoi <gabegodoi@gmail.com>
2024-07-31 18:11:56 -0700

    exercise3.py

---
+++ exercise4.py
@@ -0,0 +1,21 @@
+# forgotten quote: correct syntax error
+
+print("Hello, world!")
+
+# mismatched sock: id logic error in code meant to check if number is odd
+
+num = 7
+if num % 2 != 0:
+    print("The number is odd!")
+else:
+    print("The number is even!")
+
+# the overeager greeter: find logic error meant to greet only color red
+
+color = "pink"
+if "red" == color:
+    print("Hello, red!")
+else:
+    print("You're not red!")
+
+
